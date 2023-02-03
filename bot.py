8dc6c4c6f281dc25a4b86f187b879a14a680acf5
viizzi <82059601+viizzi@users.noreply.github.com>
2023-02-03 16:58:48 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,88 @@
+# System
+import re
+import os
+import datetime
+from config import TOKEN
+
+# Add app
+import telebot
+from pytube import YouTube
+from pytube import Playlist
+
+# Token in config.py
+token = TOKEN
+
+bot = telebot.TeleBot(token)
+
+
+def writes_logs(_ex):
+    """Записывает логи в файл 'logs.log', в котором будет время и ошибка"""
+    with open('logs.log', 'a') as file_log:
+        file_log.write('\n' + str(datetime.datetime.now()) + ': ' + str(_ex))
+
+
+def create_audio(url):
+    """Скачивает и открывает файл на бинарное чтение"""
+    try:
+        yt = YouTube(url).streams.filter(only_audio=True).first()
+        path = yt.download("music")
+        audio = open(path, 'rb')
+        return audio
+    except Exception as _ex:
+        writes_logs(_ex)
+
+
+def delete_all_music_in_directory():
+    """Удаляет все скаченные аудио из папки 'music'"""
+    if not os.path.exists('music'):
+        os.mkdir('music')
+    for file in os.listdir('music'):
+        try:
+            if re.search('mp4', file):
+                mp4_path = os.path.join('music', file)
+                os.remove(mp4_path)
+        except Exception as _ex:
+            writes_logs(_ex)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    """Стартовое приветствие"""
+    photo = open('photo/i.png', 'rb')
+    chat_id = message.chat.id
+    first_name = message.chat.first_name
+    bot.send_photo(message.chat.id, photo, f"Привет {first_name}✌\n"
+                                           f"Я могу скачать музыку с юутуба\n"
+                                           f"Пришли мне ссылку на плейлист или видео с YouTube!")
+
+
+@bot.message_handler(content_types=['text'])
+def get_files(message):
+    """Ждёт от пользователя ссылку на ютуб плейлист или видео и начинает его скачивать, и отравляет пользователю"""
+
+    if message.text[:38] == 'https://www.youtube.com/playlist?list=':
+        # Для плейлиста
+        playlist = Playlist(message.text)
+
+        for url in playlist:
+            try:
+                audio = create_audio(url)
+                bot.send_audio(message.chat.id, audio)
+            except Exception as _ex:
+                writes_logs(_ex)
+        else:
+            bot.send_message(message.chat.id, f"ООО НЕТ!!!\n"
+                                              f"Походу плейлист закрыт или вы вили неверную ссылку.")
+
+    elif message.text[:32] == 'https://www.youtube.com/watch?v=' or message.text[:17] == 'https://youtu.be/':
+        # Для видео
+        try:
+            url = message.text
+            audio = create_audio(url)
+            bot.send_audio(message.chat.id, audio)
+        except Exception as _ex:
+            writes_logs(_ex)
+
+
+delete_all_music_in_directory()
+bot.infinity_polling()
---
+++ config.py
@@ -0,0 +1,2 @@
+# Your token telegram bot
+TOKEN = '5803535129:AAEUATpFTYw3-b2_7ohvkImtCuJIQVqXpuk'
